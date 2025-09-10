f166667bd9e0ab987a6293295b0f1604ebf70386
kwang2134 <kwang2134@naver.com>
2025-09-11 03:14:47 +0900

    Initial commit

---
+++ src/test/java/com/yg/yeohaenggaja/YeohaenggajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.yeohaenggaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeohaenggajaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
