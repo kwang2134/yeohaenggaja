399213b6ece1c422f9d4896b5f17afec08c3cbff
kwang2134 <kwang2134@naver.com>
2025-09-16 18:24:01 +0900

    fix(Country): 쿼리 메서드 이름 오류로 인한 수정 EntityGraph -> JPQL 사용

--- src/main/java/com/yg/yeohaenggaja/repository/CountryRepository.java
+++ src/main/java/com/yg/yeohaenggaja/repository/CountryRepository.java
@@ -4,13 +4,14 @@ package com.yg.yeohaenggaja.repository;
 import com.yg.yeohaenggaja.domain.Country;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
-    @EntityGraph(attributePaths = {"travelSpots"})
+    @Query("SELECT c FROM Country c JOIN FETCH c.travelSpots")
     List<Country> findAllWithTravelSpots();
 
     @EntityGraph(attributePaths = {"travelSpots"})
